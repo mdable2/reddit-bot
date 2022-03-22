717b245d4c7e6ec268d608aee712466ee2f969c4
Mark Dabler <mark@tackleai.com>
2022-03-22 13:40:03 -0500

    praw works. config set up. basic sentiment and subject analysis.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # reddit-bot
+
+### Getting Started
+1. `conda create --name reddit-bot python=3.8 pip`
+2. `conda activate reddit-bot`
+3. `pip install -r requirements.txt`
---
+++ app/config.py
@@ -0,0 +1,8 @@
+HF_TOKENIZER_NEWS_CLASSIFIER = "mrm8488/bert-mini-finetuned-age_news-classification"
+HF_MODEL_NEWS_CLASSIFIER = "mrm8488/bert-mini-finetuned-age_news-classification"
+
+SUBREDDITS = "news"
+
+NUM_SUBMISSION_TO_GET = 10
+
+NEWS_CLASSES = ["world", "sports", "business", "sci/tech"]
