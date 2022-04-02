cda1839d7f1b20b34e87a20d9cbde9d6ebf5feab
Mark Dabler <mark@tackleai.com>
2022-04-01 23:23:10 -0500

    trying to clean up repo

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 HF_TOKENIZER_NEWS_CLASSIFIER = "mrm8488/bert-mini-finetuned-age_news-classification"
 HF_MODEL_NEWS_CLASSIFIER = "mrm8488/bert-mini-finetuned-age_news-classification"
 
-SUBREDDITS = "news"
+SUBREDDITS = "wallstreetbets+investing+options+pennystocks+options+stocks"
 
-NUM_SUBMISSION_TO_GET = 10
+NUM_SUBMISSION_TO_GET = 1000
 
 NEWS_CLASSES = ["world", "sports", "business", "sci/tech"]
